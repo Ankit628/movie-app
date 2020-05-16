a50e7e2e4fc459007942434180ffdf45aaeb88e6
Ankit Subedi <info@thecaesious.com>
2020-05-16 23:34:21 +0545

    Laravel-Vue Movie app -> heading styles fixed on homepage

--- resources/js/components/home/popularMovies/popularMovItems.vue
+++ resources/js/components/home/popularMovies/popularMovItems.vue
@@ -5,7 +5,7 @@
                 <img class="d-block w-100" v-bind:src="'https://image.tmdb.org/t/p/w342'+movie.poster_path" v-bind:alt="movie.title">
             </div>
             <div class="back">
-                <h3>{{movie.title}}</h3>
+                <h5>{{movie.title}}</h5>
                 <p class="d-none d-md-block">{{movie.overview.slice(0, 70)}}...</p>
                 <router-link v-bind:to="{ name: 'movie-details', params: { id: movie.id } }" class="btn btn-sm btn-secondary">Read more</router-link>
             </div>
--- resources/js/components/home/popularTv/popularTvShowsItems.vue
+++ resources/js/components/home/popularTv/popularTvShowsItems.vue
@@ -5,7 +5,7 @@
                 <img class="d-block w-100" v-bind:src="'https://image.tmdb.org/t/p/w342'+tv.poster_path" v-bind:alt="tv.name">
             </div>
             <div class="back">
-                <h3>{{tv.name}}</h3>
+                <h5>{{tv.name}}</h5>
                 <p class="d-none d-md-block">{{tv.overview.slice(0, 70)}}...</p>
                 <router-link v-bind:to="{ name: 'tv-details', params: { id: tv.id } }" class="btn btn-sm btn-secondary">Read more</router-link>
             </div>
