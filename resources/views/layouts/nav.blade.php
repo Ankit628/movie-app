a94fee7ad1726ad2d32ac6c641d60b88e6ad53e3
Ankit Subedi <info@thecaesious.com>
2020-05-16 23:38:40 +0545

    Laravel-Vue Movie app -> nav bar href added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
 <header class="sticky-top bg-dark">
     <div class="container">
         <nav class="navbar navbar-expand-lg">
-            <router-link class="navbar-brand mx-auto" v-bind:to="{ name: 'movie-details'}">
+            <a class="navbar-brand mx-auto" href="/">
                 <h2 class="h1 text-success">Movie App</h2>
-            </router-link>
+            </a>
         </nav>
     </div>
 </header>
